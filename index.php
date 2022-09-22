96541d69a270ff3cdf1ee577ce4eb575c4fe6858
Huy <nqhuy0607@gmail.com>
2022-09-22 16:08:10 +0700

    veigar

--- index.php
+++ index.php
@@ -8,8 +8,9 @@
 </head>
 <body></body>
     <?php
-    echo"<h1 style='color: pink';>Hello Jimmy</h1>";
+    echo"<h1 style='color: black';>Hello Jimmy</h1>";
     echo"<h2 style='color: red';>You have been deploy code to Heroku successfully! </h2>";
     ?>
+    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Funiverse.leagueoflegends.com%2Fvn_VN%2Fchampion%2Fveigar%2F&psig=AOvVaw3FRTexw8IdFeQwX61hs2h_&ust=1663924047691000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCLimiqSGqPoCFQAAAAAdAAAAABAD" alt="Veigar">
 </body>
 </html>
