e56515886c945e637e665fcb6861a30a29b6fe59
Huy <nqhuy0607@gmail.com>
2022-09-22 16:03:38 +0700

    new app

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quang Huy</title>
+</head>
+<body></body>
+    <?php
+    echo"<h1 style='color: pink';>Hello Jimmy</h1>";
+    echo"<h2 style='color: red';>You have been deploy code to Heroku successfully! </h2>";
+    ?>
+</body>
+</html>
